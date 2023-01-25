f025c8acba9e770f1dcad8b012f7302f4b4845fc
sohaibdar61 <sohaibdar61@gmail.com>
2023-01-25 02:07:48 +0200

    First commit of first project

---
+++ 00_CAN_ATMEGA32/MCP2515_Transceiver/01_MCAL/SPI/SPI_Interface.h
@@ -0,0 +1,19 @@
+/**********************************************************************************************************************
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *         File:  SPI_interface.h
+ *       Module:  -
+ *
+ *  Description: aLL Functions prototypes of SPI Module     
+ *  
+ *********************************************************************************************************************/
+#ifndef SPI_INTERFACE_H
+#define SPI_INTERFACE_H
+
+
+void SPI_VidMasterInit(void);
+void SPI_VidSlaveInit(void);
+void SPI_VidChipSelect(u8);
+u8 SPI_U8MasterTransmit(u8 Local_U8Data);
+
+#endif /* SPI_INTERFACE_H */
